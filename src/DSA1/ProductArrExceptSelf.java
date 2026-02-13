fdb35aed75ec5a7ef5ad544eba74388e915215a9
Garima Bansal <bansalgarima689@gmail.com>
2026-02-13 23:49:49 +0530

    commit-msg

---
+++ src/DSA1/ProductArrExceptSelf.java
@@ -0,0 +1,40 @@
+package DSA1;
+
+public class ProductArrExceptSelf {
+
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+
+        // Left products
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+
+        // Right products
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] = res[i] * right;
+            right *= nums[i];
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+
+        int[] result = productExceptSelf(nums);
+
+        // Print output
+        System.out.print("Output: [");
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i]);
+            if (i != result.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+}
