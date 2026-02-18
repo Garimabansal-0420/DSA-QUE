2e2b92c72e2176ff234fc2c8bfab07ea019b3684
Garima Bansal <bansalgarima689@gmail.com>
2026-02-18 23:29:45 +0530

    commit-msg

--- src/Linked_List/SingleLL.java
+++ src/Linked_List/SingleLL.java
@@ -36,7 +36,7 @@ public class SingleLL {
 }
 
 */
-
+/*
 package Linked_List;
 import java.util.*;
 
@@ -99,5 +99,111 @@ public class SingleLL {
     }
     n1.insertatstart(100);
         n1.display();
+
+        deletenode();
+        n1.display();
+    }
+}
+static void deletenode(int data){
+
+}*/
+
+package Linked_List;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class linkedlist {
+    Node head;
+
+    void insert(int data) {
+        Node newnode = new Node(data);
+
+        if (head == null) {
+            head = newnode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newnode;
+    }
+
+    void insertatstart(int data) {
+        Node newnode2 = new Node(data);
+
+        if (head == null) {
+            head = newnode2;
+            return;
+        }
+
+        newnode2.next = head;
+        head = newnode2;
+    }
+
+    void deletenode(int data) {
+
+        if (head == null) return;
+
+        // If head needs to be deleted
+        if (head.data == data) {
+            head = head.next;
+            return;
+        }
+        Node temp = head;
+
+        // Find node before the one to delete
+        while (temp.next != null) {
+            if (temp.next.data == data) {
+                temp.next = temp.next.next;
+                return;
+            }
+            temp = temp.next;
+        }
+    }
+
+
+    void display() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+}
+
+public class SingleLL {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        linkedlist n1 = new linkedlist();
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            n1.insert(sc.nextInt());
+        }
+
+        n1.insertatstart(100);
+
+        n1.display();
+
+        int deleteValue = sc.nextInt(); // value to delete
+        n1.deletenode(deleteValue);
+
+        n1.display();
+
     }
 }
